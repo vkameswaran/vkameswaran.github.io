a8d39e6fb7b47a4a8915cb3daf29722ffd02f7b0
vkameswaran <vaishantkameswaran@gmail.com>
2023-02-03 15:52:06 -0500

    Updates

---
+++ precache-manifest.ef1a805a53dffc5b0e1204c4c4df00ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83bc088259479ac6313dbdaa2465390",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ccae3cb46e5d190ddba",
+    "url": "/static/css/main.98ff587f.chunk.css"
+  },
+  {
+    "revision": "09426f891d9bb2d878ae",
+    "url": "/static/js/2.ee11cb2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ee11cb2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ccae3cb46e5d190ddba",
+    "url": "/static/js/main.9439daf7.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  }
+]);
